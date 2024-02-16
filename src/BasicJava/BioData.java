53387d01fb96c800576b227129360201a93de3e9
Shazzad01 <shazzadm065@gmail.com>
2024-02-16 15:27:21 +0600

    Java Learning

---
+++ src/BasicJava/BioData.java
@@ -0,0 +1,11 @@
+package BasicJava;
+
+public class BioData {
+    public static void main(String[] args){
+        System.out.println("Shazzad \n01254687954 \nBsc. in CSE  ");
+        System.out.println("1 \t 2");
+        System.out.println("3 \t 4");
+        System.out.println("\"Java Programing\"");
+
+    }
+}
